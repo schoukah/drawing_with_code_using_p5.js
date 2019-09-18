c9317ad339ca4dcccf55b2e36011711d9ed4fc1c
Adam Herst <adamherst@adamherst.com>
2019-09-18 11:39:27 -0400

    Minimally working version of animating placement of circle.

--- 25_animating_the_placement_of_shapes/sketch.js
+++ 25_animating_the_placement_of_shapes/sketch.js
@@ -5,7 +5,8 @@ Varying the colour of shapes
 */
 
 // variables for ellipse
-var circleX, circleY, circleWidth, circleHeight;
+var circleXOld, circleYOld, circleWidthOld, circleHeightOld;
+var circleXNew, circleYNew, circleWidthNew, circleHeightNew;
 
 // variables for rect
 var squareX, squareY, squareWidth, squareHeight;
@@ -15,43 +16,26 @@ var triangleX1, triangleY1, triangleX2, triangleY2, triangleX3, triangleY3;
 
 // variables for colour
 var colours = ['red','yellow','blue'];
+shuffleArray(colours);
+
+var isDone = 0;
 
 function setup() {
   createCanvas(400, 400);
   fill(255,255,255,128);
-  frameRate(1);
-}
-
-function draw() {
-  background('lightgrey');
-
-  drawGraph();
-
-  stroke('black');
-  strokeWeight(0);
-  shuffleArray(colours);
-
-  // draw the circle
+//  frameRate(1);
   circleWidthOld = random(200);
-  circleHeightOld = circleWidth;
-  circleXOld = circleWidthOld/2 + random(400-circleWidthOld);
-  circleYOld = circleHeightOld/2 + random(400-circleHeightOld);
+  circleHeightOld = circleWidthOld;
+  circleXOld = int(circleWidthOld/2 + random(400-circleWidthOld));
+  circleYOld = int(circleHeightOld/2 + random(400-circleHeightOld));
   circleWidthNew = random(200);
   circleHeightNew = circleWidthNew;
-  circleXNew = circleWidthNew/2 + random(400-circleWidthNew);
-  circleYNew = circleHeightNew/2 + random(400-circleHeightNew);
-  fill(colours[0]);
-  ellipse(circleXNew, circleYNew, circleWidthNew, circleHeightNew);
-
-  // draw the square
+  circleXNew = int(circleWidthNew/2 + random(400-circleWidthNew));
+  circleYNew = int(circleHeightNew/2 + random(400-circleHeightNew));
   squareWidth = random(200);
   squareHeight = squareWidth;
   squareX = random(400-squareWidth);
   squareY = random(400-squareHeight);
-  fill(colours[1]);
-  rect(squareX, squareY, squareWidth, squareHeight);
-
-  // draw the triangle
   triangleX1 = 150;
   triangleY1 = 25;
   triangleX2 = 290;
@@ -72,8 +56,36 @@ function draw() {
   triangleY1 = triangleY1 + (triangleNewCentreY - triangleCentreY);
   triangleY2 = triangleY2 + (triangleNewCentreY - triangleCentreY);
   triangleY3 = triangleY3 + (triangleNewCentreY - triangleCentreY);
+
+}
+
+function draw() {
+  background('lightgrey');
+
+  drawGraph();
+
+  stroke('black');
+  strokeWeight(0);
+
+  // draw the circle
+
+
+  fill(colours[0]);
+    ellipse(circleXOld, circleYOld, circleWidthOld, circleHeightOld);
+  circleXOld += (circleXNew-circleXOld)/10;;
+  circleYOld += (circleYNew-circleYOld)/10;
+  console.log(circleXOld, circleYOld);
+
+
+  // draw the square
+  fill(colours[1]);
+  rect(squareX, squareY, squareWidth, squareHeight);
+
+  // draw the triangle
+
   fill(colours[2]);
   triangle(triangleX1, triangleY1, triangleX2, triangleY2, triangleX3, triangleY3);
+
 }
 
 function drawGraph() {
@@ -90,8 +102,8 @@ function drawGraph() {
 
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
