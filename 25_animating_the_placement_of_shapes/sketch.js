9189b2cbc255bfb2e6f6063ea18e946e6984223a
Adam Herst <adamherst@adamherst.com>
2019-09-27 11:28:53 -0400

    Minimally working version of animating placement of triangle.

--- 25_animating_the_placement_of_shapes/sketch.js
+++ 25_animating_the_placement_of_shapes/sketch.js
@@ -13,11 +13,12 @@ var squareXStart, squareYStart, squareWidthStart, squareHeightStart;
 var squareXStop, squareYStop, squareWidthStop, squareHeightStop;
 
 // variables for triangle
-var triangleX1, triangleY1, triangleX2, triangleY2, triangleX3, triangleY3;
+var triangleX1Start, triangleY1Start, triangleX2Start, triangleY2Start, triangleX3Start, triangleY3Start;
+var triangleX1Stop, triangleY1Stop, triangleX2Stop, triangleY2Stop, triangleX3Stop, triangleY3Stop;
+var triangleScale = 1;
 
 // variables for colour
 var colours = ['red','yellow','blue'];
-shuffleArray(colours);
 
 var circleIsDone = false;
 var squareIsDone = false;
@@ -27,6 +28,7 @@ function setup() {
   createCanvas(400, 400);
   fill(255,255,255,128);
 
+  // set the initial parameters for the circle
   circleWidthStart = random(200);
   circleHeightStart = circleWidthStart;
   circleXStart = int(circleWidthStart/2 + random(400-circleWidthStart));
@@ -36,6 +38,7 @@ function setup() {
   circleXStop = int(circleWidthStop/2 + random(400-circleWidthStop));
   circleYStop = int(circleHeightStop/2 + random(400-circleHeightStop));
 
+  // set the initial parameters for the square
   squareWidthStart = random(200);
   squareHeightStart = squareWidthStart;
   squareXStart = int(squareWidthStart/2 + random(400-squareWidthStart));
@@ -45,26 +48,34 @@ function setup() {
   squareXStop = int(squareWidthStop/2 + random(400-squareWidthStop));
   squareYStop = int(squareHeightStop/2 + random(400-squareHeightStop));
 
-  triangleX1 = 150;
-  triangleY1 = 25;
-  triangleX2 = 290;
-  triangleY2 = 100;
-  triangleX3 = 150;
-  triangleY3 = 175;
+  // set the initial paramerts for the triangle
+  triangleX1Start = 150;
+  triangleY1Start = 25;
+  triangleX2Start = 290;
+  triangleY2Start = 100;
+  triangleX3Start = 150;
+  triangleY3Start = 175;
 
   // find the centroid of the triangle
-  triangleCentreX = (triangleX1 + triangleX2 + triangleX3)/3;
-  triangleCentreY = (triangleY1 + triangleY2 + triangleY3)/3;
+  triangleCentreXStart = (triangleX1Start + triangleX2Start + triangleX3Start)/3;
+  triangleCentreYStart = (triangleY1Start + triangleY2Start + triangleY3Start)/3;
 
-  triangleNewCentreX = random(400);
-  triangleNewCentreY = random(400);
+  // generate a random point to be the new centroid
+  triangleCentreXStop = random(100,300);
+  triangleCentreYStop = random(100,300);
 
-  triangleX1 = triangleX1 + (triangleNewCentreX - triangleCentreX);
-  triangleX2 = triangleX2 + (triangleNewCentreX - triangleCentreX);
-  triangleX3 = triangleX3 + (triangleNewCentreX - triangleCentreX);
-  triangleY1 = triangleY1 + (triangleNewCentreY - triangleCentreY);
-  triangleY2 = triangleY2 + (triangleNewCentreY - triangleCentreY);
-  triangleY3 = triangleY3 + (triangleNewCentreY - triangleCentreY);
+  // generate new coordinates that maintain the shape of the original triangle
+  // generate a number to scale the distance between the vertices and the new centroid
+  // random value between 0.1 and the shortest distance between the centroid and the four sides
+  var triangleScale = random(0.1,2);
+
+  // calculate the coordinates of the new triangle
+  triangleX1Stop = (triangleX1Start + (triangleCentreXStop - triangleCentreXStart)) * triangleScale;
+  triangleX2Stop = (triangleX2Start + (triangleCentreXStop - triangleCentreXStart)) * triangleScale;
+  triangleX3Stop = (triangleX3Start + (triangleCentreXStop - triangleCentreXStart)) * triangleScale;
+  triangleY1Stop = (triangleY1Start + (triangleCentreYStop - triangleCentreYStart)) * triangleScale;
+  triangleY2Stop = (triangleY2Start + (triangleCentreYStop - triangleCentreYStart)) * triangleScale;
+  triangleY3Stop = (triangleY3Start + (triangleCentreYStop - triangleCentreYStart)) * triangleScale;
 }
 
 function draw() {
@@ -78,8 +89,8 @@ function draw() {
   // draw the circle
   fill(colours[0]);
   ellipse(circleXStart, circleYStart, circleWidthStart, circleHeightStart);
-  circleXStart += (circleXStop-circleXStart)/10;
-  circleYStart += (circleYStop-circleYStart)/10;
+  circleXStart += (circleXStop-circleXStart)/100;
+  circleYStart += (circleYStop-circleYStart)/100;
   if (circleXStop - circleXStart < 1 && circleYStop - circleYStart < 1) {
     circleIsDone = true;
   }
@@ -87,15 +98,22 @@ function draw() {
   // draw the square
   fill(colours[1]);
   rect(squareXStart, squareYStart, squareWidthStart, squareHeightStart);
-  squareXStart += (squareXStop-squareXStart)/10;
-  squareYStart += (squareYStop-squareYStart)/10;
+  squareXStart += (squareXStop-squareXStart)/100;
+  squareYStart += (squareYStop-squareYStart)/100;
   if (squareXStop - squareXStart < 1 && squareYStop - squareYStart < 1) {
     squareIsDone = true;
   }
 
   // draw the triangle
   fill(colours[2]);
-  triangle(triangleX1, triangleY1, triangleX2, triangleY2, triangleX3, triangleY3);
+  triangle(triangleX1Start, triangleY1Start, triangleX2Start, triangleY2Start, triangleX3Start, triangleY3Start);
+  triangleX1Start += (triangleX1Stop - triangleX1Start)/100 * triangleScale;
+  triangleX2Start += (triangleX2Stop - triangleX2Start)/100 * triangleScale;
+  triangleX3Start += (triangleX3Stop - triangleX3Start)/100 * triangleScale;
+  triangleY1Start += (triangleY1Stop - triangleY1Start)/100 * triangleScale;
+  triangleY2Start += (triangleY2Stop - triangleY2Start)/100 * triangleScale;
+  triangleY3Start += (triangleY3Stop - triangleY3Start)/100 * triangleScale;
+
 
   // if the shapes are in their new positions
   // generate new stop coordinates and start again
@@ -104,6 +122,12 @@ function draw() {
     circleYStop = int(circleHeightStop/2 + random(400-circleHeightStop));
     squareXStop = int(squareWidthStop/2 + random(400-squareWidthStop));
     squareYStop = int(squareHeightStop/2 + random(400-squareHeightStop));
+    triangleX1Stop = (triangleX1Stop+ (triangleCentreXStop - triangleCentreXStart));
+    triangleX2Stop = (triangleX2Stop+ (triangleCentreXStop - triangleCentreXStart));
+    triangleX3Stop = (triangleX3Stop+ (triangleCentreXStop - triangleCentreXStart));
+    triangleY1Stop = (triangleY1Stop+ (triangleCentreYStop - triangleCentreYStart));
+    triangleY2Stop = (triangleY2Stop+ (triangleCentreYStop - triangleCentreYStart));
+    triangleY3Stop = (triangleY3Stop+ (triangleCentreYStop - triangleCentreYStart));
     circleIsDone = false;
     squareIsDone = false;
   }
